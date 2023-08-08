cf8b59219c19381cc6d719895bb4ef143ffb1f80
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-08 23:01:45 +0500

    update app.js

--- app.js
+++ app.js
@@ -4,3 +4,8 @@
 // var num1 = Math.round(num);
 // var num2 = Math.floor(num);
 // var num3 = Math.ceil(num);
+
+// document.write("Number : " +  num + "<br>");
+// document.write("Round : " +  num1 + "<br>");
+// document.write("Floor : " +  num2 + "<br>");
+// document.write("Ceil : " +  num3 + "<br>");
