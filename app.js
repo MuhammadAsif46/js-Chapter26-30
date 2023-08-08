68fddfa1da768ead26fdae7e2b75f8e113ce8c38
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-08 23:01:20 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert("hello world");
+//*********Question 1 *********//
+
+// var num = +prompt("Enter positive a number.");
+// var num1 = Math.round(num);
+// var num2 = Math.floor(num);
+// var num3 = Math.ceil(num);
